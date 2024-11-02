f718e83ff9cbf8963beabd4d92ac0fefeaf5256f
Muhib Dulquer Shohel <muhibdulquershohel@gmail.com>
2024-11-03 00:02:24 +0600

    completed the voting program and tests

--- tests/voting.ts
+++ tests/voting.ts
@@ -1,16 +1,26 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Voting } from "../target/types/voting";
+import { startAnchor } from "solana-bankrun";
+import { BankrunProvider } from "anchor-bankrun";
+import { PublicKey } from "@solana/web3.js";
 
+const votingAddress = new PublicKey("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ")
+const IDL = require('../target/idl/voting.json');
 describe("voting", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.Voting as Program<Voting>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Is initialized poll", async () => {
+    const context = await startAnchor("", [{ name: "voting", programId: votingAddress }], [])
+    const provider = new BankrunProvider(context)
+    const votingProgram = new Program<Voting>(IDL, provider)
+
+    await votingProgram.methods.initializePoll(
+      new anchor.BN(1),
+      "What is your favourite type of peanut butter",
+      new anchor.BN(1),
+      new anchor.BN(13842384623864),
+
+    ).rpc()
   });
 });
